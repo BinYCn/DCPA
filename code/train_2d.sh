564e4758e400a7ca9236bce3612e1ba1ab069deb
BinYCn <52229894+BinYCn@users.noreply.github.com>
2024-01-08 15:23:11 +0800

    Update train_2d.sh

--- code/train_2d.sh
+++ code/train_2d.sh
@@ -1,7 +1,7 @@
-python train_2d.py  --model DCPA2d --labelnum 14 --gpu 0 && \
-python train_2d.py  --model DCPA2d --labelnum 7 --gpu 0 && \
-python train_2d.py  --model DCPA2d --labelnum 3 --gpu 0 && \
+python train_2d.py  --model DCPA2d --labeled_num 14 --gpu 0 && \
+python train_2d.py  --model DCPA2d --labeled_num 7 --gpu 0 && \
+python train_2d.py  --model DCPA2d --labeled_num 3 --gpu 0 && \
 
-python test_2d.py --exp DCPA2d --model DCPA2d --labelnum 14 --gpu 0 && \
-python test_2d.py --exp DCPA2d --model DCPA2d --labelnum 7 --gpu 0 && \
-python test_2d.py --exp DCPA2d --model DCPA2d --labelnum 3 --gpu 0
+python test_2d.py --exp DCPA2d --model DCPA2d --labeled_num 14 --gpu 0 && \
+python test_2d.py --exp DCPA2d --model DCPA2d --labeled_num 7 --gpu 0 && \
+python test_2d.py --exp DCPA2d --model DCPA2d --labeled_num 3 --gpu 0
